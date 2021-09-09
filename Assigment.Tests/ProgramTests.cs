d55d54d792d8f601afe30dc01f3825571c0647e4
Werenfeldt <63417041+Werenfeldt@users.noreply.github.com>
2021-09-09 11:20:35 +0200

    Feature leap year (#2)
    * Tested and Implemented IsLeadYear
    
    * Created user interface for IsLeapYear
    
    * Implemented error handling
    
    * without a test because it waits for an enter press
    
    * Added documentation.

---
+++ Assigment.Tests/ProgramTests.cs
@@ -0,0 +1,117 @@
+using System;
+using Xunit;
+using System.IO;
+using System.Windows.Input;
+
+namespace Assigment.Tests
+{
+    public class ProgramTests
+    {
+
+        [Fact]
+        public void IsLeapYear_devisible_by_four()
+        {
+
+            bool x = new Program(40).IsLeapYear(40);
+
+            Assert.True(x);
+        }
+
+        [Fact]
+        public void IsLeapYear_not_devisible_by_four()
+        {
+
+            bool x = new Program(77).IsLeapYear(77);
+
+            Assert.False(x);
+        }
+
+        [Fact]
+        public void IsLeapYear_devisible_by_four_not_100()
+        {
+            bool x = new Program(80).IsLeapYear(80);
+
+            Assert.True(x);
+        }
+
+        [Fact]
+        public void IsLeapYear_is_devisible_by_four_100()
+        {
+            bool x = new Program(200).IsLeapYear(200);
+
+            Assert.False(x);
+        }
+
+        [Fact]
+        public void IsLeapYear_is_devilisble_by_four_100_400()
+        {
+            bool x = new Program(800).IsLeapYear(800);
+
+            Assert.True(x);
+        }
+
+
+        [Fact]
+        public void IsLeapYear_input()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+
+            new Program(2000);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("yay", output);
+
+        }
+
+        [Fact]
+        public void IsNOTLeapYear_input()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+
+            new Program(7777);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("nay", output);
+
+        }
+
+        [Fact]
+        public void IsLeapYear_input_valid_year()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+
+            new Program(600);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("The year must be at least 1582", output);
+        }
+
+        //keeps waiting for an enter press. 
+        /*
+        [Fact]
+        public void IsLeapYear_input_Invalid()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Console.SetIn(new StringReader("hej"));
+
+
+            //needs to press enter to run this test. 
+            Program.Main(new string[0]);
+
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+
+
+            Assert.Equal("The input must be an integer", output);
+        }
+        */
+    }
+}
--- Assigment/Program.cs
+++ Assigment/Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.IO;
 
 namespace Assigment
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    public class Program
+    { 
+
+        public int year;
+        public bool leapYear;
+        public string answer;
+
+        public static void Main(string[] args){
+
+            //makes sure the terminal doesnt echo the input. 
+            string input = null;
+            while (true)
+            {
+                var key = System.Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter)
+                    break;
+                input += key.KeyChar;
+            }
+
+            int year;
+            if(int.TryParse(input, out year)){
+                new Program(year);
+            } else {
+                Console.WriteLine("The input must be an integer");
+            }
+            
+        }
+
+        public Program(int year){
+
+            if(year < 1582){
+                Console.WriteLine("The year must be at least 1582");
+            } else {
+                this.leapYear = IsLeapYear(year);   
+            
+                this.answer = response(leapYear);
+
+                Console.WriteLine(answer);
+            }
+        }
+
+        public bool IsLeapYear(int year){
+
+            if(year % 4 != 0){
+                return false; 
+            } else {
+                if(year % 100 == 0) {
+                    if(year % 400 == 0){
+                        return true;
+                    } else {
+                        return false; 
+                    }
+                } else {
+                    return true;
+                }
+            }
+        }
+
+        public string response(bool IsLeapYear){
+            if(IsLeapYear){
+                return "yay";
+            } else {
+                return "nay";
+            }
         }
     }
 }
